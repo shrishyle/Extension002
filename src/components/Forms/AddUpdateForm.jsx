f08c32dd535e652a4e9db2705be36d3ad54084b9
shrishyle pandit <shrishyle.pandit@gmail.com>
2026-01-11 23:27:41 +0530

    Created Modify Task Form

--- src/components/Forms/AddUpdateForm.jsx
+++ src/components/Forms/AddUpdateForm.jsx
@@ -1,5 +1,28 @@
+import Button from "../Button";
+import InputElement from "../InputElement";
+import { FormDisplayContext } from "../../store/form-display-context";
+import { useContext } from "react";
+
 const AddUpdateForm = () => {
-  return <div>AddUpdateForm</div>;
+  const { clearSidebar, show_create_new_task_btn_func } = useContext(FormDisplayContext);
+
+  return (
+    <>
+      <div className="w-full h-12 bg-teal-700 flex flex-col justify-center border mt-2 rounded-sm text-teal-50 text-center">Add Update</div>
+      <InputElement label={`Task Title`} name={`Task Title`} type={`text`} propClass={``} />
+      <InputElement label={`Task Title`} name={`Task Title`} type={`text`} propClass={``} />
+      <Button type={`button`} propClasses={``} label={`Add Update`} clickHandler="" />
+      <Button
+        type={`button`}
+        propClasses={``}
+        label={`Cancel`}
+        clickHandler={() => {
+          clearSidebar();
+          show_create_new_task_btn_func();
+        }}
+      />
+    </>
+  );
 };
 
 export default AddUpdateForm;
