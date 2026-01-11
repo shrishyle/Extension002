9ef76a4c644284801d2ae8caa7c73b989530542b
shrishyle pandit <shrishyle.pandit@gmail.com>
2026-01-11 23:22:20 +0530

    Deleted "create-new-task-button" component which was redundant

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import CreateNewTaskBtn from "./CreateNewTaskBtn";
 import CreateNewTaskForm from "./Forms/CreateNewTaskForm";
 import { FormDisplayContext } from "../store/form-display-context";
 import Button from "./Button";
@@ -7,11 +6,20 @@ import ModifyTaskForm from "./Forms/ModifyTaskForm";
 import React from "react";
 
 const Sidebar = () => {
-  const { formDisplay, show_modify_task_form_func, show_add_update_form_func, show_modify_update_form_func, clearSidebar } = useContext(FormDisplayContext);
+  const { formDisplay, show_modify_task_form_func, show_add_update_form_func, show_modify_update_form_func, clearSidebar, show_create_new_task_form_func } = useContext(FormDisplayContext);
 
   return (
     <div className="w-1/7 min-h-screen bg-teal-50 flex flex-col items-center justify-top shadow-md text-grey-500 p-1">
-      {formDisplay.showCreateNewTaskBtn && <CreateNewTaskBtn />}
+      {formDisplay.showCreateNewTaskBtn && (
+        <Button
+          type="button"
+          label="Create New Task"
+          clickHandler={() => {
+            clearSidebar();
+            show_create_new_task_form_func();
+          }}
+        />
+      )}
       {formDisplay.showCreateNewTaskForm && <CreateNewTaskForm />}
       {formDisplay.showModifyTaskBtn && (
         <Button
