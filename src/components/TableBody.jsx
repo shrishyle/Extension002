f6c02e932f967f75dc2d6b9ebfbee163f0aa06da
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2026-02-01 22:27:23 +0530

    update changes

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,6 +1,10 @@
-import { Fragment } from "react";
-import React from "react";
+import React, { useContext } from "react";
+import { FormDisplayContext } from "../store/form-display-context";
+import { TaskContext } from "../store/task-context";
+
 const TableBody = ({ data, taskCategory }) => {
+  const { show_modify_task_btn_func, show_add_update_btn_func, show_modify_update_btn_func, clearSidebar } = useContext(FormDisplayContext);
+
   let taskToDisplay = data.filter((task) => task.taskCategory === taskCategory);
   let defaultCalsses = "border cursor-pointer border-gray-300";
 
@@ -12,19 +16,52 @@ const TableBody = ({ data, taskCategory }) => {
             <td key={`${taskIndex}101${task.taskDescription}`} rowSpan={task.taskUpdate.length + 1} className=" border border-gray-300 align-middle text-center">
               {`${taskIndex + 1}.`}
             </td>
-            <td key={`${taskIndex}102${task.taskDescription}`} rowSpan={task.taskUpdate.length + 1} className={`pr-3 pl-3 ${defaultCalsses}`}>
+            <td
+              key={`${taskIndex}102${task.taskDescription}`}
+              rowSpan={task.taskUpdate.length + 1}
+              className={`pr-3 pl-3 ${defaultCalsses}`}
+              onClick={() => {
+                clearSidebar();
+                show_add_update_btn_func();
+                show_modify_task_btn_func();
+              }}
+            >
               {task.taskDescription}
             </td>
           </tr>
           {task.taskUpdate.map((update, updateIndex) => (
             <tr key={`${taskIndex}${updateIndex}8`}>
-              <td key={`${taskIndex}${updateIndex}90`} className={`text-center pr-2 pl-2 ${defaultCalsses}`}>
+              <td
+                key={`${taskIndex}${updateIndex}90`}
+                className={`text-center pr-2 pl-2 ${defaultCalsses}`}
+                onClick={() => {
+                  clearSidebar();
+                  show_add_update_btn_func();
+                  show_modify_update_btn_func();
+                }}
+              >
                 {update[0]}
               </td>
-              <td key={`${taskIndex}${updateIndex}91`} className={`pr-2 pl-2 ${defaultCalsses}`}>
+              <td
+                key={`${taskIndex}${updateIndex}91`}
+                className={`pr-2 pl-2 ${defaultCalsses}`}
+                onClick={() => {
+                  clearSidebar();
+                  show_add_update_btn_func();
+                  show_modify_update_btn_func();
+                }}
+              >
                 {update[1]}
               </td>
-              <td key={`${taskIndex}${updateIndex}92`} className={`pr-2 pl-2 ${defaultCalsses}`}>
+              <td
+                key={`${taskIndex}${updateIndex}92`}
+                className={`pr-2 pl-2 ${defaultCalsses}`}
+                onClick={() => {
+                  clearSidebar();
+                  show_add_update_btn_func();
+                  show_modify_update_btn_func();
+                }}
+              >
                 {update[2]}
               </td>
             </tr>
