4b7f535ad34ba3fcb211711eac085886b9431d93
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2026-02-09 06:54:00 +0530

    Rectified Form Design

--- src/components/Forms/ModifyUpdateForm.jsx
+++ src/components/Forms/ModifyUpdateForm.jsx
@@ -4,7 +4,6 @@ const ModifyUpdateForm = () => {
       <div className="w-full h-12 bg-teal-700 flex flex-col justify-center border mt-2 rounded-sm text-teal-50 text-center">Modify your Task</div>
       <InputElement label={`Task Title`} name={`Task Title`} type={`text`} propClass={``} />
       <SelectionInputElement label={`Select Task Category`} id={`select_task_category`} taskCategories={uniqueTaskCategories} />
-      <SelectionInputElement label={`Select Task Status`} id={`select_task_status`} taskCategories={taskStatus} />
       <Button type={`button`} propClasses={``} label={`Modify Task`} clickHandler={modifyTask} />
       <Button type={`button`} propClasses={``} label={`Delete Task`} clickHandler={deleteTask} />
       <Button
